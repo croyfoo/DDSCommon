4ef95700a092d7fc35ecaa17f07291952e4e994e
David Croy <djcroy@mac.com>
2022-10-09 17:40:34 -0400

    Fixed an issue what the date extension was defined twice

--- Sources/Extensions/Shared/Data+Extensions.swift
+++ Sources/Extensions/Shared/Data+Extensions.swift
@@ -79,15 +79,4 @@ public extension Data {
     }
 }
 
-extension Date: RawRepresentable {
-    private static let formatter = ISO8601DateFormatter()
-    
-    public var rawValue: String {
-        Date.formatter.string(from: self)
-    }
-    
-    public init?(rawValue: String) {
-        self = Date.formatter.date(from: rawValue) ?? Date()
-    }
-}
 
