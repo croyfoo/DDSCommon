7e32dec6ec6c5619154071e096ebdd1af3fe640a
David Croy <djcroy@mac.com>
2022-10-09 17:57:11 -0400

    Added Color+Extension

--- Sources/Extensions/Shared/Collection+Extension.swift
+++ Sources/Extensions/Shared/Collection+Extension.swift
@@ -8,6 +8,6 @@
 
 import Foundation
 
-extension Collection {
+public extension Collection {
     var isNotEmpty: Bool { isEmpty == false }
 }
---
+++ Sources/Extensions/Shared/Color+Extension.swift
@@ -0,0 +1,33 @@
+//
+//  Color+Extension.swift
+//  Tes
+//
+//  Created by David Croy on 6/5/21.
+//  Copyright © 2021 DoubleDog Software. All rights reserved.
+//
+
+import SwiftUI
+
+public extension Color {
+    
+    #if os(macOS)
+    static let background          = Color(.windowBackgroundColor)
+    static let secondaryBackground = Color(.underPageBackgroundColor)
+    static let tertiaryBackground  = Color(.controlBackgroundColor)
+    static let textBackground      = Color(.textBackgroundColor)
+    #else
+    static let background          = Color(.systemBackground)
+    static let secondaryBackground = Color(.secondarySystemBackground)
+    static let tertiaryBackground  = Color(.tertiarySystemBackground)
+    #endif
+    
+    static let normal       = Color(white: 0.0, opacity: 0.0) // 0
+    static let darkHover    = Color(white: 1.0, opacity: 0.25)// 1
+    static let darkClicked  = Color(white: 1.0, opacity: 0.4) // 2
+    static let lightHover   = Color(white: 0.0, opacity: 0.25) //7
+    static let lightClicked = Color(white: 0.0, opacity: 0.6) //8
+
+    static let foregroundNormal     = Color(white: 0.75, opacity: 1.0) //4
+    static let foregroundDarkHover  = Color(white: 1.0, opacity: 1.0) //5
+    static let foregroundLightHover = Color(white: 0.2, opacity: 1.0) //6
+}
