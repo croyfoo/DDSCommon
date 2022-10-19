58d640d00a416396bcb73dbcf1efe1c26dfd6ea5
David Croy <djcroy@mac.com>
2022-10-18 23:52:43 -0400

    Updated the shadow radius

--- Sources/SwiftUI/extensions/View+Extensions.swift
+++ Sources/SwiftUI/extensions/View+Extensions.swift
@@ -24,7 +24,7 @@ public extension View {
             )
             .if(shadow) { view in
                 view.clipped()
-                    .shadow(radius: shadowRadius)
+                    .shadow(radius: 3)
             }
     }
     
