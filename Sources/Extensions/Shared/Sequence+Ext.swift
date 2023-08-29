ff5e70cbf3012d45add088713ecc74d2f6b2e5c8
David Croy <djcroy@mac.com>
2023-08-29 14:05:57 -0400

    Added a unique extension for Sequence

---
+++ Sources/Extensions/Shared/Sequence+Ext.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by David Croy on 8/29/23.
+//
+
+import Foundation
+
+extension Sequence where Iterator.Element: Hashable {
+    func unique() -> [Iterator.Element] {
+        var seen: Set<Iterator.Element> = []
+        return filter { seen.insert($0).inserted }
+    }
+}
