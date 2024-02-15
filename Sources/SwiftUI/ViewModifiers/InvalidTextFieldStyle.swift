a87af67e7922b7e72568aeb905f03cde50ebc3f9
David Croy <djcroy@mac.com>
2024-02-15 14:07:00 -0500

    Added the invalidTextFieldStyle from the FinancialT app

---
+++ Sources/SwiftUI/ViewModifiers/InvalidTextFieldStyle.swift
@@ -0,0 +1,27 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by David Croy on 2/15/24.
+//
+
+import SwiftUI
+
+struct InvalidTextFieldStyle: TextFieldStyle {
+    private var isValid           = false
+    private var changeEffectValue = 0
+    
+    init(_ isValid: Bool = true) {
+        self.isValid = isValid
+        changeEffectValue += !isValid ? 1 : 0
+    }
+    
+    func _body(configuration: TextField<Self._Label>) -> some View {
+        configuration
+            .overlay {
+                RoundedRectangle(cornerRadius: 8, style: .continuous)
+                    .stroke(isValid ? .clear : .red, lineWidth: 2)
+            }
+        //            .changeEffect(.shake(rate: .fast), value: changeEffectValue)
+    }
+}
