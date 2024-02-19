5337ffa5bcab600d40792cab559f79d9c9d56e2a
David Croy <djcroy@mac.com>
2024-02-19 14:52:49 -0500

    Fixed the merge issues

--- Sources/SwiftUI/Views/PDFKitView.swift
+++ Sources/SwiftUI/Views/PDFKitView.swift
@@ -6,13 +6,55 @@
 //
 
 import SwiftUI
+import PDFKit
 
-struct SwiftUIView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+#if os(macOS)
+typealias representable = NSViewRepresentable
+#else
+typealias representable = UIViewRepresentable
+#endif
+
+public struct PDFKitView: representable {
+    public typealias UIViewType = PDFView
+    
+    var data: Data
+    let singlePage: Bool
+    
+    init(_ data: Data, singlePage: Bool = false) {
+        self.data       = data
+        self.singlePage = singlePage
     }
-}
-
-#Preview {
-    SwiftUIView()
+    
+#if os(iOS)
+    public func makeUIView(context: Context) -> UIViewType {
+        makeView(context: context)
+    }
+    
+    public func updateUIView(_ view: UIViewType, context: Context) {
+        //        updateView(view, context: context)
+    }
+#else
+    public func makeNSView(context: Context) -> UIViewType {
+        makeView(context: context)
+    }
+    
+    public func updateNSView(_ view: UIViewType, context: Context) {
+        updateView(view, context: context)
+    }
+#endif
+    
+    private func makeView(context: Context) -> UIViewType {
+        // Create a `PDFView` and set its `PDFDocument`.
+        let pdfView        = PDFView()
+        pdfView.document   = PDFDocument(data: data)
+        pdfView.autoScales = true
+        if singlePage {
+            pdfView.displayMode = .singlePage
+        }
+        return pdfView
+    }
+    
+    private func updateView(_ view: UIViewType, context _: Context) {
+        view.document = PDFDocument(data: data)
+    }
 }
