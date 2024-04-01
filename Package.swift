21f3f3231c4c4312a93953e331bf2542cfde348d
David Croy <djcroy@mac.com>
2024-04-01 18:50:36 -0400

    Updated the tools to support macOS 14 and IOS 17

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.7
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 // Use macOS V12.
 
@@ -7,8 +7,8 @@ import PackageDescription
 let package = Package(
     name: "DDSCommon",
     platforms: [
-        .macOS(.v13),
-        .iOS(.v16)
+        .macOS(.v14),
+        .iOS(.v17)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
