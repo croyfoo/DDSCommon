e88f6f3ead1ed6488608849ca50021d597f962d4
David Croy <djcroy@mac.com>
2024-04-22 15:38:03 -0400

    Added the RotateImage and renamed it to rotate since it should work on anyview.

---
+++ Sources/SwiftUI/ViewModifiers/RotateImage.swift
@@ -0,0 +1,52 @@
+//
+//  RotateImage.swift
+//  FinancialT
+//
+//  Created by David Croy on 9/13/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct Rotate: ViewModifier {
+    @State private var degrees: Double
+    private let startDegrees: Double
+    private let rotationDegres: Double
+    private let gesture: any Gesture
+    
+    init(_ startDegrees: Double = -90.0, gesture: any Gesture = TapGesture()) {
+        self.startDegrees   = startDegrees
+        self._degrees       = State(initialValue: startDegrees)
+        self.rotationDegres = startDegrees - 360
+        self.gesture        = gesture
+    }
+    
+    func body(content: Content) -> some View {
+        content
+            .rotationEffect(.degrees(degrees))
+//            .gesture(
+//                (self.gesture as Gesture)
+//                self.gesture { _ in
+//                    withAnimation(.easeIn(duration: 0.5)) {
+//                        degrees = (degrees == rotationDegres) ? startDegrees : rotationDegres
+//                    }
+//                }
+//            )
+            .onTapGesture {
+                withAnimation(.easeIn(duration: 0.5)) {
+                    degrees = (degrees == rotationDegres) ? startDegrees : rotationDegres
+                }
+            }
+    }
+}
+
+extension View {
+    func rotate(_ startDegrees: Double = -90) -> some View {
+        self.modifier(Rotate(startDegrees))
+    }
+}
+
+#Preview {
+    Text("Hello, World!")
+        .rotate()
+}
